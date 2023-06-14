83a1fdb77594fbf1f3ad416e5a0e3d5c26acb5ab
samisohail85 <128447830+samisohail85@users.noreply.github.com>
2023-06-14 11:41:26 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "project_id" {
+  type        = string
+  description = "The Project ID"
+  default = "fiery-outpost-381205"
+}
+variable "subnetwork-region" {
+  default = "sub-tf-1"
+}
+
+variable "network" {
+  default = "test"
+}
